32a75b95325cf2fcf60adec31be42f105c0b0aa2
yuchen <yuchenz27@outlook.com>
2023-03-28 09:40:36 +0800

    added query all saved maps functionality

--- Assets/Scripts/ARWorldMapController.cs
+++ Assets/Scripts/ARWorldMapController.cs
@@ -17,9 +17,16 @@ public class ARWorldMapController : MonoBehaviour
 
     [SerializeField] private TMP_Text _promptText;
 
+    [SerializeField] private MapSlot _mapSlotPrefab;
+
+    [SerializeField] private RectTransform _mapRoot;
+
+    [SerializeField] private GameObject _mapScroll;
+
     private void Start()
     {
         _promptWindow.SetActive(false);
+        _mapScroll.SetActive(false);
     }
 
     public void SaveCurrentWorldMap()
@@ -80,5 +87,65 @@ public class ARWorldMapController : MonoBehaviour
     {
         yield return new WaitForSeconds(3f);
         _promptWindow.SetActive(false);
+    }
+
+    public void OnMapsButtonPressed()
+    {
+        if (_mapScroll.activeSelf)
+        {
+            _mapScroll.SetActive(false);
+        }
+        else
+        {
+            QueryMaps();
+            _mapScroll.SetActive(true);
+        }
+    }
+
+    private void QueryMaps()
+    {
+        for (int i = 0; i < _mapRoot.childCount; i++)
+        {
+            Destroy(_mapRoot.GetChild(i).gameObject);
+        }
+
+        string[] files = Directory.GetFiles(Application.persistentDataPath);
+
+        foreach (string file in files)
+        {
+            Debug.Log(file);
+            string[] arr1 = file.Split("/");
+            string[] arr2 = arr1[arr1.Length - 1].Split(".");
+            string date = arr2[0];
+
+            FileInfo fileInfo = new FileInfo(file);
+            string size = (fileInfo.Length / 1024f / 1024f).ToString("F2");
+
+            var mapSlotInstance = Instantiate(_mapSlotPrefab);
+            mapSlotInstance.Date.text = "Date: " + date;
+            mapSlotInstance.Size.text = "Size: " + size + "mb";
+            mapSlotInstance.LoadButton.onClick.AddListener(() =>
+            {
+                LoadWorldMap(file);
+            });
+            mapSlotInstance.DeleleButton.onClick.AddListener(() =>
+            {
+                DeleteWorldMap(file);
+            });
+
+            mapSlotInstance.transform.SetParent(_mapRoot);
+            mapSlotInstance.transform.localScale = Vector3.one;
+        }
+    }
+
+    private void LoadWorldMap(string file)
+    {
+        _mapScroll.SetActive(false);
+
+    }
+
+    private void DeleteWorldMap(string file)
+    {
+
     }
 }
---
+++ Assets/Scripts/MapSlot.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class MapSlot : MonoBehaviour
+{
+    public TMP_Text Date;
+
+    public TMP_Text Size;
+
+    public Button LoadButton;
+
+    public Button DeleleButton;
+}
